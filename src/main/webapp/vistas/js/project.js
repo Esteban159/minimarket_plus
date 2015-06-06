301bf4eba894e1b7e6080c01a6735747eb58239b
Esteban159 <esteban.adrian.159@gmail.com>
2015-06-06 13:09:11 -0500

    actualizacion

---
+++ src/main/webapp/vistas/js/project.js
@@ -0,0 +1,24 @@
+function changeIframe1() {
+  	var ventana = document.getElementById('myIframe');
+    ventana.src = "Inventario.html";
+}
+
+function changeIframe3() {
+  	var ventana = document.getElementById('myIframe');
+	ventana.src = "historial.html";
+}
+
+function changeIframe4() {
+  	var ventana = document.getElementById('myIframe');
+    ventana.src = "contabilidad.html";
+}
+
+function changeIframe5() {
+  	var ventana = document.getElementById('myIframe');
+	ventana.src = "reg_productos.html";
+}
+
+function changeIframe6() {
+    var ventana = document.getElementById('myIframe');
+  ventana.src = "atencion.html";
+}
